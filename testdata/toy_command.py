f012886b49abdd93ada13f2f8c806f0fbd8053fb
Chenying Zhao <chenyzh@seas.upenn.edu>
2022-08-17 15:23:39 -0400

    add python file as the core script of toy BIDS App

---
+++ testdata/toy_command.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# ref: https://github.com/bids-apps/example/blob/master/run.py
+
+import argparse
+import os
+import subprocess
+#import nibabel
+#import numpy
+#from glob import glob
+
+def run(command, env={}):
+    merged_env = os.environ
+    merged_env.update(env)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT, shell=True,
+                               env=merged_env)
+    while True:
+        line = process.stdout.readline()
+        line = str(line, 'utf-8')[:-1]
+        print(line)
+        if line == '' and process.poll() != None:
+            break
+    if process.returncode != 0:
+        raise Exception("Non zero return code: %d"%process.returncode)
+
+parser = argparse.ArgumentParser(description='Example BIDS App entrypoint script.')
+parser.add_argument('bids_dir', help='The directory with the input dataset '
+                    'formatted according to the BIDS standard.')
+parser.add_argument('output_dir', help='The directory where the output files '
+                    'should be stored. If you are running group level analysis '
+                    'this folder should be prepopulated with the results of the'
+                    'participant level analysis.')
+parser.add_argument('analysis_level', help='Level of the analysis that will be performed. '
+                    'Multiple participant level analyses can be run independently '
+                    '(in parallel) using the same output_dir.',
+                    choices=['participant', 'group'])
+parser.add_argument('--participant_label', '--participant-label',
+                    help='The label of the participant that should be analyzed. The label '
+                    'can either be <sub-xx>, or just <xx> which does not include "sub-"')
+parser.add_argument('--session_label', '--session-label', '--ses_label', '--ses-label',
+                    help='The label of the session that should be analyzed. The label '
+                    'can either be <ses-xx>, or just <xx> which does not include "ses-"')
+
+args = parser.parse_args()
+
+
+if args.participant_label:
+    if "sub-" == args.participant_label[0:4]:
+        participant_label = args.participant_label
+    else:
+        participant_label = "sub-" + args.participant_label
+
+print("participant: " + participant_label)
+
+if args.session_label:
+    if "ses-" == args.session_label[0:4]:
+        session_label = args.session_label
+    else:
+        session_label = "ses-" + args.session_label
+
+print("session: " +session_label)
+
+if args.analysis_level == "participant":
+    dir_4analysis = os.path.join(args.bids_dir, participant_label, session_label)
+
+    # print(dir_4analysis)
+    print('Recursively counting number of non-hidden files in: ', dir_4analysis)
+
+    num_files = 0
+    for base, dirs, files in os.walk(dir_4analysis):
+        
+        for Files in files:
+            if "." == Files[0]:    # if it's a hidden file, skip
+                continue
+            else:
+                #print(Files)
+                num_files += 1
+    
+    print(num_files)
+
+    # save the number into an output.csv:
+    fn_output = os.path.join(args.output_dir, "num_nonhidden_files.txt")
+    with open(fn_output, 'w') as f:
+        f.write(str(num_files))
+
+
+
+
+print()
+    
