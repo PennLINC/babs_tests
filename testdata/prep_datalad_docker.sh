330928dd64ba288ba00f4c088f01582af5bee865
Chenying Zhao <chenyzh@seas.upenn.edu>
2022-08-17 10:35:19 -0400

    push to docker hub

--- testdata/prep_datalad_docker.sh
+++ testdata/prep_datalad_docker.sh
@@ -9,13 +9,16 @@ datalad_version="0.17.2"
 # first, cd to where this bash file locates
 
 # build:
-docker build -t chenyingzhao/datalad:${datalad_version} -f Dockerfile_datalad \
+docker build -t chenyingzhao/datalad:datalad${datalad_version} -f Dockerfile_datalad \
     --build-arg datalad_version=${datalad_version} . 
 
 # test:
-docker run --rm -ti chenyingzhao/datalad:${datalad_version}
+docker run --rm -ti chenyingzhao/datalad:datalad${datalad_version}
 # in the container, try out:
     # $ datalad --version
     # $ git --version
     # $ git-annex
     # $ datalad create -c text2git my-dataset
+
+# push:
+docker push chenyingzhao/datalad:datalad${datalad_version}
