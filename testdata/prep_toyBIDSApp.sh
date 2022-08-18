c7bd82f31ba885eaf7f5eb1948e3dbc1730dbcc1
Chenying Zhao <chenyzh@seas.upenn.edu>
2022-08-18 10:04:20 -0400

    add multi architecture build

--- testdata/prep_toyBIDSApp.sh
+++ testdata/prep_toyBIDSApp.sh
@@ -2,12 +2,14 @@
 
 # first, cd to the folder containing this bash script (and Dockerfile + toy_command.sh)
 
-version_tag="0.0.2"
+version_tag="0.0.2.9000"
+version_tag_dash="0-0-2-9000"
 
-# build:
+# Build:
+# first, test building with regular builder: (later we will use multi-architecture builder to build the image and push)
 docker build -t chenyingzhao/toy_bids_app:${version_tag} -f Dockerfile_toyBIDSApp .
 
-# test:
+# Test:
 input_dir="/Users/chenyzh/Desktop/Research/Satterthwaite_Lab/datalad_wrapper/data/data_hashedID"
 mounted_input_dir="/mnt/testdir"
 output_dir="/Users/chenyzh/Desktop/Research/Satterthwaite_Lab/datalad_wrapper/data"
@@ -23,5 +25,30 @@ docker run --rm -ti -v ${input_dir}:${mounted_input_dir} \
     --participant_label ${participant_label} \
     --session_label ${session_label}
 
-# push to Docker Hub:
-docker push chenyingzhao/toy_bids_app:${version_tag}
+# Push to Docker Hub:
+# docker push chenyingzhao/toy_bids_app:${version_tag}  
+# ^^ this is for linux system; on Mac M1, we need to use multi-architecture, 
+# so that docker image built on Mac M1 can be run on other architectures e.g., cubic with amd64
+
+# ref: https://docs.docker.com/desktop/multi-arch/
+docker buildx use mybuilder   # use the builder which gives access to the new multi-architecture features. | created by: $ docker buildx create --name mybuilder
+docker buildx build --platform linux/amd64,linux/arm64 --push -t chenyingzhao/toy_bids_app:${version_tag} -f Dockerfile_toyBIDSApp .
+
+# Test on cubic cluster using singularity
+# first, build singularity image:
+cmd="singularity build toybidsapp-${version_tag}.sif docker://chenyingzhao/toy_bids_app:${version_tag}"
+
+# test running:
+input_dir="/cbica/projects/RBC/chenying_practice/data_for_babs/NKI/data_hashedID_noDataLad"
+output_dir="/cbica/projects/RBC/chenying_practice/data_for_babs/NKI"
+folder_sif="/cbica/projects/RBC/chenying_practice/software"
+singularity run --cleanenv -B ${PWD}  \
+    ${folder_sif}/toybidsapp-${version_tag}.sif \
+    $input_dir \
+    $output_dir \
+    participant \
+    --participant-label $participant_label \
+    --session_label ${session_label}
+
+
+# -B means "bind"
