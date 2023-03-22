c104e2cdb77abbc76680c20449ba003d24d7a894
Chenying Zhao <chenyzh@seas.upenn.edu>
2023-03-22 12:50:31 -0400

    build docker image used for circle ci tests of BABS: add singularity

---
+++ testdata/prep_docker_babs_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This is to build the docker image used for Circle CI tests of BABS
+
+# +++++++++++++++++++++++++++++++
+datalad_version="0.17.2"
+datalad_osf_version="0.2.3.1"
+datalad_container_version="1.1.6"
+
+docker_tag="datalad${datalad_version}_v2"
+# +++++++++++++++++++++++++++++++
+
+# build:
+docker build -t chenyingzhao/datalad:${docker_tag} -f Dockerfile_babs_tests \
+    --build-arg datalad_version=${datalad_version} \
+    --build-arg datalad_osf_version=${datalad_osf_version} \
+    --build-arg datalad_container_version=${datalad_container_version} . 
+
